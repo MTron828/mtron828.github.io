debbb83ada6a69ace2f2a125ca2042146a7eeb63
mtron828 <mtron828@gmail.com>
2024-07-18 20:59:23 +0200

    Deleted novels

--- novels.py
+++ novels.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+from progress_bar import printProgressBar
 
 json_cache = {}
 def loadJson(file):
@@ -58,23 +58,61 @@ def giveIdsToNovels():
     for name in data:
         getIdFromName(name)
 
-def getChapterCount(id):
+def getChapterCountNovelbin(id):
     filenum = id//100
     offset = id%100
     file = loadJson("./novelbin_links_{}.json".format(filenum))
     return len(file[offset])
 
-def getAiChapterCount(id):
+def getAiChapterCountOsPath(id):
     path = "./novels/{}/chapters/ai".format(id)
     if os.path.isdir(path):
         return sum(1 for entry in os.listdir(path) if os.path.isfile(os.path.join(path, entry)))
     else:
         return 0
 
-def getNovelDescription(id):
+def getNovelDescriptionNovelbin(id):
     data = loadJson("./novelbin_data.json")
     return data[getNameFromId(id)]["description"]
 
-def getNovelTags(id):
+def getNovelTagsNovelbin(id):
     data = loadJson("./novelbin_data.json")
     return data[getNameFromId(id)]["tags"]  
+
+def precalc_novel_data():
+    novelbin_data = loadJson("./novelbin_data.json")
+    names = getIdToName()
+    data = loadJson("novel_data.json")
+    for i in range(1, len(names)):
+        if len(data) == i:
+            data.append({})
+        elif data[i] != {}:
+            continue
+        name = getNameFromId(i)
+        src = None
+        description = None
+        tags = []
+        chapters = None
+        chaptersAi = None
+        if name in novelbin_data:
+            src = "novelbin"
+            description = getNovelDescriptionNovelbin(i)
+            tags = getNovelTagsNovelbin(i)
+            chapters = getChapterCountNovelbin(i)
+            chaptersAi = getAiChapterCountOsPath(i)
+
+        def isMissing(field):
+            return (not (field in data[i])) or (data[i][field] == None)
+
+        def replaceIfMissing(field, value):
+            if isMissing(field) and value != None:
+                data[i][field] = value
+        
+        replaceIfMissing("name", name)
+        replaceIfMissing("source", src)
+        replaceIfMissing("description", description)
+        replaceIfMissing("tags", tags)
+        replaceIfMissing("chapters", chapters)
+        replaceIfMissing("chaptersAi", chaptersAi)
+        printProgressBar(i, len(names)-1, prefix = "Progress: ", suffix = "{} of {}".format(i, len(names)-1), length = 30)
+    storeJson("novel_data.json", data)
