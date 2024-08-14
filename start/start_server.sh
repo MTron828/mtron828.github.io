df813f8ec264d6e39eecbba33c630f3e27dcb8fd
MTron828 <mtron828@gmail.com>
2024-08-14 11:49:23 +0200

    Usage data

--- start/start_server.sh
+++ start/start_server.sh
@@ -1,2 +1,7 @@
-cd ..
+cd /home/sergio/Documentos/GitHub/mtron828.github.io/
+#echo exit | xauth -f /home/sergio/.Xauthority
+#export $(grep -v '^#' ./start/env.env | xargs)
+export XAUTHORITY=$(xauth info | awk -F"Authority file:" '{print $2}' | tr -d ' ' | head -1)
+export DISPLAY=:0
+#google-chrome
 ./bin/python app.py
